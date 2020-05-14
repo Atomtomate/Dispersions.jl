6f190e3d063cd29e9f91d310f5bb0bf2c2e33a1c
Julian Stobbe <Atomtomate@gmx.de>
2020-05-14 15:49:16 +0200

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ makedocs(
     format = Documenter.HTML(),
     modules = [Dispersions]
 )
-
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
