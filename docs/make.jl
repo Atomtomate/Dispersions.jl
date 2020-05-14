d3eaab9d3f4771818c92f431456ec756164c0640
Julian Stobbe <Atomtomate@gmx.de>
2020-05-14 15:18:15 +0200

    docs 10

--- docs/make.jl
+++ docs/make.jl
@@ -11,5 +11,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "https://github.com/Atomtomate/Dispersions.jl.git"
+    repo = "github.com/Atomtomate/Dispersions.jl.git",
+    branch = "gh-pages"
 )
