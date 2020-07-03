aa196ccecf05b3c9034256df326c924174e04ab7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-03 12:37:16 +0000

    build based on 352a45c

--- stable/search_index.js
+++ stable/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Dispersions.jl-1","page":"Dispersions.jl","title":"Dispersions.jl","text":"","category":"section"},{"location":"#","page":"Dispersions.jl","title":"Dispersions.jl","text":"Documentation for Dispersions.jl","category":"page"}]
+[{"location":"#Dispersions.jl","page":"Dispersions.jl","title":"Dispersions.jl","text":"","category":"section"},{"location":"","page":"Dispersions.jl","title":"Dispersions.jl","text":"Documentation for Dispersions.jl","category":"page"}]
 }
