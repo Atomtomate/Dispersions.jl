fc7ac063662e777466ee0fcfe784a8a061208380
Julian Stobbe <Atomtomate@gmx.de>
2021-04-30 15:23:22 +0200

    dim argument for kintegrate

--- src/BZIntegration.jl
+++ src/BZIntegration.jl
@@ -11,10 +11,12 @@ struct KSum <: KSpaceIntegrator end
 
 kintegrate(grid::Nothing, arr::AbstractArray) = arr
 
-function kintegrate(grid::T1, arr::AbstractArray; type::T2 = KSum()) where {T1 <: ReducedKGrid, T2 <: KSum}
-    return sum(grid.kMult .* arr)/sum(grid.kMult)
+function kintegrate(grid::T1, arr::AbstractArray; dim=1, type::T2 = KSum()) where {T1 <: ReducedKGrid, T2 <: KSum}
+    size(arr)[dim] != length(grid.kMult) && throw(ArgumentError("Dimension does not seem to be on a k grid! Length is $(size(arr)[dim]) but should be $(length(grid.kMult))."))
+    return mapslices(sub_arr -> sum(grid.kMult .* sub_arr)/sum(grid.kMult), arr, dims=dim)
 end
 
-function kintegrate(grid::T1, arr::AbstractArray; type::T2 = KSum()) where {T1 <: FullKGrid, T2 <: KSum}
+function kintegrate(grid::T1, arr::AbstractArray; dim=1, type::T2 = KSum()) where {T1 <: FullKGrid, T2 <: KSum}
+    !all(size(grid.ϵkGrid) .== size(arr)) && throw(ArgumentError("Array does not seem to be on a k grid! Size is $(size(arr)) but should be $(size(grid.ϵkGrid))."))
     return sum(arr)/length(arr)
 end
--- test/BZIntegration.jl
+++ test/BZIntegration.jl
@@ -6,5 +6,9 @@
         gr3_r = reduceKGrid(gr3)
         @test kintegrate(gr2, ones(size(gr2.ϵkGrid))) ≈ 1
         @test kintegrate(gr3, ones(size(gr3.ϵkGrid))) ≈ 1
+        @test_throws ArgumentError kintegrate(gr2, ones(7,8))
+        @test_throws ArgumentError kintegrate(gr2_r, ones(7,8))
+        @test all(kintegrate(gr2_r, ones(length(gr2_r.ϵkGrid)..., 4), dim=1) .≈ ones(4))
+        @test all(kintegrate(gr3_r, ones(length(gr3_r.ϵkGrid)..., 4), dim=1) .≈ ones(4))
     end
 end
