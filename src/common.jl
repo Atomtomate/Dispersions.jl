e49d817139a6f0c3680f6172f7400c3234bafcea
Julian Stobbe <Atomtomate@gmx.de>
2021-07-24 12:49:09 +0200

    ifft_post bugfix

--- src/common.jl
+++ src/common.jl
@@ -83,7 +83,7 @@ function conv_fft1(kG::ReducedKGrid{T}, arr1::AbstractArray{Complex{Float64},1},
     reshape(fft(expandKArr(kG, arr1))[:] .* arr2, gridshape(kG)) |> ifft |> x-> reduceKArr(kG, ifft_post!(T, x)) ./ Nk(kG)
 end
 
-function conv_fft(kG::ReducedKGrid, arr1::AbstractArray{Complex{Float64},1}, arr2::AbstractArray{Complex{Float64},1}) where T <: KGridType
+function conv_fft(kG::ReducedKGrid{T}, arr1::AbstractArray{Complex{Float64},1}, arr2::AbstractArray{Complex{Float64},1}) where T <: KGridType
     Nk(kG) == 1 && return arr1 .* arr2
     reshape(arr1 .* arr2, gridshape(kG)...) |> ifft |> x-> reduceKArr(kG, ifft_post!(T,x )) ./ Nk(kG)
 end
