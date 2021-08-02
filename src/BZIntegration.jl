177b9a10ef889883612782741f203706575b4e4f
Julian Stobbe <Atomtomate@gmx.de>
2021-08-02 21:08:18 +0200

    kintegrate optimized version

--- src/BZIntegration.jl
+++ src/BZIntegration.jl
@@ -9,11 +9,15 @@ abstract type KSpaceIntegrator end
 struct KSum <: KSpaceIntegrator end
 
 
-kintegrate(grid::Nothing, arr::AbstractArray) = arr
+kintegrate(kG::Nothing, arr::AbstractArray) = arr
 
-function kintegrate(grid::T1, arr::AbstractArray; dim=1, type::T2 = KSum()) where {T1 <: ReducedKGrid, T2 <: KSum}
-    size(arr)[dim] != length(grid.kMult) && throw(ArgumentError("Dimension does not seem to be on a k grid! Length is $(size(arr)[dim]) but should be $(length(grid.kMult))."))
-    return mapslices(sub_arr -> sum(grid.kMult .* sub_arr)/sum(grid.kMult), arr, dims=dim)
+function kintegrate(kG::T1, arr::AbstractArray, dim::Int, type::T2 = KSum()) where {T1 <: ReducedKGrid, T2 <: KSum}
+    size(arr)[dim] != length(kG.kMult) && throw(ArgumentError("Dimension does not seem to be on a k grid! Length is $(size(arr)[dim]) but should be $(length(kG.kMult))."))
+    return mapslices(sub_arr -> sum(kG.kMult .* sub_arr)/Nk(kG), arr, dims=dim)
+end
+
+function kintegrate(kG::T1, arr::AbstractArray; type::T2 = KSum()) where {T1 <: ReducedKGrid, T2 <: KSum}
+    return sum(kG.kMult .* arr)/Nk(kG)
 end
 
 #function kintegrate(grid::T1, arr::AbstractArray; dim=1, type::T2 = KSum()) where {T1 <: FullKGrid, T2 <: KSum}
