7483d3581db686b2279e3484dc7b75743af3015c
MLeusch <marvin.m.leusch@gmail.com>
2022-05-20 17:34:06 +0200

    FCC reduction, currently doesnt work

--- src/cF.jl
+++ src/cF.jl
@@ -14,12 +14,27 @@ basis_transform(::Type{cF}, v::Tuple) =
     Tuple([-1.0 1.0 1.0; 1.0 -1.0 1.0; 1.0 1.0 -1.0] * collect(v))
 
 function reduce_KGrid(::Type{cF}, D::Int, Ns::Int, kGrid::AbstractArray)
-    ind = collect(Base.product([1:Ns for Di = 1:3]...))
-    kMult = ones(length(ind))
-    expand_perms = map(x -> [CartesianIndex{3}(x)], ind[:])
+    (D != 3) && throw(
+        ArgumentError(
+            "FCC lattice only exists in 3 dimensions!",
+        ),
+    )
+    ind = collect(Base.product([1:Ns for Di = 1:D]...))
+    parents, ops = find_classes(fccSymmetries, kGrid[:], UInt32.(repeat([1],48)));
+    kmap, grid_red = minimal_set(parents, kGrid[:]);  
+    kMult = zeros(Int64,length(grid_red))
+    expand_perms = Vector{Vector{NTuple{D, Int}}}(undef,length(grid_red))
+    for i in 1:length(grid_red)
+        kMult[i] = length(fccSymmetries(grid_red[i]))
+        expand_perms[i] = fccSymmetries(grid_red[i])
+    end
+
+    #ind = collect(Base.product([1:Ns for Di = 1:3]...))
+    #kMult = ones(length(ind))
+    #expand_perms = map(x -> [CartesianIndex{3}(x)], ind[:])
     red_map = CartesianIndex.(ind[:])
     red_conv_map = reverse(red_map)
-    return red_map, red_conv_map, kMult, expand_perms, kGrid[:]
+    return red_map, red_conv_map, kMult, expand_perms, grid_red
 end
 
 gen_ϵkGrid(::Type{cF}, kGrid::GridPoints, t::T) where {T<:Real} = collect(
@@ -67,4 +82,20 @@ function build_expand_mapping_cF(D::Int, Ns::Int, ind_red::Array)
         kMult[ri] = length(expand_perms[ri])
     end
     return kMult, expand_perms
+end
+
+function fccSymmetries(k)
+	symm = Array{Vector{Float64},1}(undef, 48)
+	perms = collect(permutations(k))
+	for i in 1:length(perms)
+		symm[8*(i-1)+1] = perms[i] .* [1,1,1]
+		symm[8*(i-1)+2] = perms[i] .* [1,1,-1]
+		symm[8*(i-1)+3] = perms[i] .* [1,-1,1]
+		symm[8*(i-1)+4] = perms[i] .* [1,-1,-1]
+		symm[8*(i-1)+5] = perms[i] .* [-1,1,1]
+		symm[8*(i-1)+6] = perms[i] .* [-1,1,-1]
+		symm[8*(i-1)+7] = perms[i] .* [-1,-1,1]
+		symm[8*(i-1)+8] = perms[i] .* [-1,-1,-1]
+	end
+	return unique(symm)[:]
 end
