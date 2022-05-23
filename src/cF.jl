4c36c713e5485cf95c495f5cb405099fa2b95292
MLeusch <marvin.m.leusch@gmail.com>
2022-05-23 15:57:50 +0200

    cF Tests don't error but still fail

--- src/cF.jl
+++ src/cF.jl
@@ -19,22 +19,27 @@ function reduce_KGrid(::Type{cF}, D::Int, Ns::Int, kGrid::AbstractArray)
             "FCC lattice only exists in 3 dimensions!",
         ),
     )
-    ind = collect(Base.product([1:Ns for Di = 1:D]...))
-    parents, ops = find_classes(fccSymmetries, kGrid[:], UInt32.(repeat([1],48)));
-    kmap, grid_red = minimal_set(parents, kGrid[:]);  
-    kMult = zeros(Int64,length(grid_red))
-    expand_perms = Vector{Vector{NTuple{D, Int}}}(undef,length(grid_red))
-    for i in 1:length(grid_red)
-        kMult[i] = length(fccSymmetries(grid_red[i]))
-        expand_perms[i] = fccSymmetries(grid_red[i])
+    fsymm(kInd) = fccSymmetries(kInd,Ns)
+    ind = collect(Base.product([1:Ns for Di = 1:3]...))
+    parents, ops = find_classes(fsymm, vec(ind), UInt32.(repeat([1],12)));
+    kmap, ind_red = minimal_set(parents, vec(ind));
+    println(ind_red)
+    grid_red = Array{NTuple{3,Float64},1}(undef,length(ind_red))
+    for (i,indi) in enumerate(ind_red)
+        grid_red[i] = kGrid[CartesianIndex(indi)]
     end
-
+    kMult = zeros(Int64,length(ind_red))
+    expand_perms = Vector{Vector{CartesianIndex{D}}}(undef,length(ind_red))
+    for i in 1:length(ind_red)
+        kMult[i] = length(fsymm(ind_red[i]))
+        expand_perms[i] = CartesianIndex.(fsymm(ind_red[i]))
+    end
     #ind = collect(Base.product([1:Ns for Di = 1:3]...))
     #kMult = ones(length(ind))
     #expand_perms = map(x -> [CartesianIndex{3}(x)], ind[:])
-    red_map = CartesianIndex.(ind[:])
-    red_conv_map = reverse(red_map)
-    return red_map, red_conv_map, kMult, expand_perms, grid_red
+    #red_map = CartesianIndex.(ind[:])
+    red_conv_map = CartesianIndex.(reverse(ind_red))
+    return CartesianIndex.(ind_red), red_conv_map, kMult, expand_perms, grid_red
 end
 
 gen_ϵkGrid(::Type{cF}, kGrid::GridPoints, t::T) where {T<:Real} = collect(
@@ -84,18 +89,12 @@ function build_expand_mapping_cF(D::Int, Ns::Int, ind_red::Array)
     return kMult, expand_perms
 end
 
-function fccSymmetries(k)
-	symm = Array{Vector{Float64},1}(undef, 48)
-	perms = collect(permutations(k))
-	for i in 1:length(perms)
-		symm[8*(i-1)+1] = perms[i] .* [1,1,1]
-		symm[8*(i-1)+2] = perms[i] .* [1,1,-1]
-		symm[8*(i-1)+3] = perms[i] .* [1,-1,1]
-		symm[8*(i-1)+4] = perms[i] .* [1,-1,-1]
-		symm[8*(i-1)+5] = perms[i] .* [-1,1,1]
-		symm[8*(i-1)+6] = perms[i] .* [-1,1,-1]
-		symm[8*(i-1)+7] = perms[i] .* [-1,-1,1]
-		symm[8*(i-1)+8] = perms[i] .* [-1,-1,-1]
-	end
-	return unique(symm)[:]
+function fccSymmetries(kind,Ns)    
+    symm = Array{NTuple{3,Int64},1}(undef, 12)
+    perms = collect(permutations(kind.-1))
+    for i in 1:length(perms)
+        symm[2*(i-1)+1] = Tuple(perms[i] .+ 1)
+        symm[2*(i-1)+2] = Tuple(((Ns-1) .- perms[i]) .+1)
+    end
+    return unique(symm)[:]
 end
