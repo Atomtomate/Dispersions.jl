f992e627cfa5c28dc76d60d222fba848040a4c41
Julian Stobbe <Atomtomate@gmx.de>
2023-03-13 17:18:17 +0100

    sign fix hofstadter

--- src/cPnn_mag.jl
+++ src/cPnn_mag.jl
@@ -8,8 +8,10 @@ abstract type Hofstadter{P,Q} <: KGridType end
 #                            Grid Generator Functions                              #
 # -------------------------------------------------------------------------------- #
 
-gen_sampling(::Type{Hofstadter{P,Q}}, D::Int, Ns::Int) where {P,Q} = Base.product([[(2*j - Ns)/(2*Ns) for j in 0:Ns-1] for Di in 1:2]...)
-basis_transform(::Type{Hofstadter{P,Q}}, v::Tuple) where {P,Q} = (2π * v[1], 2π*v[2]/Q)
+gen_sampling(::Type{Hofstadter{P,Q}}, D::Int, Ns::Int) where {P,Q} =
+    Base.product([[(2 * π / Ns) * j - π for j = 1:Ns] for Di = 1:D]...)
+
+basis_transform(::Type{Hofstadter{P,Q}}, v::Tuple) where {P,Q} = (v[1], v[2]/Q)
 
 function reduce_KGrid(::Type{Hofstadter{P,Q}}, D::Int, Ns::Int, kGrid::AbstractArray) where {P,Q}
     ind = collect(Base.product([1:Ns for Di in 1:2]...))
@@ -21,21 +23,24 @@ function reduce_KGrid(::Type{Hofstadter{P,Q}}, D::Int, Ns::Int, kGrid::AbstractA
 end
 
 function gen_ϵkGrid_Hofstadter_ij(::Type{Hofstadter{P,Q}}, i::Int, j::Int, kᵢ::NTuple{2,Float64}, t::Float64) where {P,Q}
+    res = 0.0
     if i==j
-        return -2*t*(cos(kᵢ[1] + 2*π*P*(i-1)/Q))
-    elseif i == j+1 || i == j-1
-        return -t
-    elseif i == Q && j == 1
-        return t*exp(-1im*Q*kᵢ[2])
-    elseif j == Q && i == 1
-        return t*exp(1im*Q*kᵢ[2])
-    else
-        return 0
+        res += -2*t*(cos(kᵢ[1] + 2*π*P*(i-1)/Q))
     end
+    if i == j+1 || i == j-1
+        res += -t
+    end
+    if i == Q && j == 1
+        res += -t*exp(-1im*Q*kᵢ[2])
+    end
+    if j == Q && i == 1
+        res += -t*exp(1im*Q*kᵢ[2])
+    end
+    return res
 end
 
 function gen_ϵkGrid(::Type{Hofstadter{P,Q}}, kGrid::GridPoints, t::Float64, tp::Float64, tpp::Float64) where {P,Q}
-    if gcd(P,Q) != 1 || Q < 3
+    if !(P==1 && Q==1) && (gcd(P,Q) != 1 || Q < 3)
         throw(ArgumentError("gcd(P,Q) = $(gcd(P,Q)) != 1 or Q = $(Q) < 3!"))
     end
     res = Array{ComplexF64,3}(undef, Q, Q, length(kGrid))
