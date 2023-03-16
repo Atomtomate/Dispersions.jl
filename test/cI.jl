cf74ad79ce8eeee166df01d1388b536dbcce0470
Julian Stobbe <Atomtomate@gmx.de>
2023-03-16 11:51:11 +0100

    tests for bcc

--- test/cI.jl
+++ test/cI.jl
@@ -8,19 +8,6 @@ include("helper_functions.jl")
     indTest = reduceKArr(r8, reshape([(1, 1, 1) (2, 1, 1) (1, 2, 1) (2, 2, 1) (1, 1, 2) (2, 1, 2) (1, 2, 2) (2, 2, 2)], (2,2,2)))
     gridTest = reduceKArr(r8, reshape([(0, 0, 0) (π, 0, π) (π, π, 0) (2π, π, π) (0, π, π) (π, π, 2π) (π, 2π, π) (2π, 2π, 2π)], (2,2,2)))
     
-    for kG in [r8,r64]
-        eps_k = expandKArr(kG,kG.ϵkGrid)
-        full_k_grid = expandKArr(kG,kG.kGrid)
-        comp_disp_ekgrid = falses((gridshape(kG)...,gridshape(kG)...))
-        #=for k in CartesianIndices(full_k_grid)
-            for q in CartesianIndices(full_k_grid)
-                comp_disp_ekgrid[k,q] = isapprox(fcc_dispersion(full_k_grid[k] .+ full_k_grid[q],kG.t), eps_k[mod(k[1]+q[1]-2,kG.Ns)+1,mod(k[2]+q[2]-2,kG.Ns)+1,mod(k[3]+q[3]-2,kG.Ns)+1],atol=num_eps)
-                #println(string(comp_disp_ekgrid[k,q])*" Disp = "*string(fcc_dispersion(full_k_grid[k] .+ full_k_grid[q],kG.t))*" EpsGrid = "*string(eps_k[mod(k[1]+q[1]-2,kG.Ns)+1,mod(k[2]+q[2]-2,kG.Ns)+1,mod(k[3]+q[3]-2,kG.Ns)+1])*" k="*string(full_k_grid[k])*" q="*string(full_k_grid[q]))
-            end
-        end
-        @test all(comp_disp_ekgrid)=#
-    end
-
     @test Nk(r8) == 2^3
     @test Nk(r64) == 4^3
     @test all(dispersion(r8) .≈ r8.ϵkGrid)
