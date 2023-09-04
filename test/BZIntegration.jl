627869d71f6d064a99ffa8d6628dbb6d32a6dcde
Jan Frederik Weissler <jfwmaster@outlook.de>
2023-09-04 11:44:29 +0200

    Add BZIntegration tests
    Comparisons with analytically known integrals for the 3d simple cubic lattice.
    
    Note: These are not unit tests but integration tests because the dispersion relation from the project is used.

--- test/BZIntegration.jl
+++ test/BZIntegration.jl
@@ -7,4 +7,15 @@
             @test all(kintegrate(gr, ones(length(gr.ϵkGrid)..., 4), 1) .≈ ones(4))
         end
     end
+
+    # --- check against analytic properties of the 3d simple cubic lattice ---
+    # Integral of the dispersion raised to odd powers vanishes. This can be shown by induction. For the indution step one may use integration by reduction.
+    kGr = gen_kGrid("3dsc-$(1/(2*sqrt(6)))", 20)
+    @test abs(kintegrate(kGr, kGr.ϵkGrid))    < 10^(-10) 
+    @test abs(kintegrate(kGr, kGr.ϵkGrid.^3)) < 10^(-10)
+    @test abs(kintegrate(kGr, kGr.ϵkGrid.^5)) < 10^(-10)
+    # Integral of the dispersion raised to even powers can be evaluated by hand
+    @test kintegrate(kGr, kGr.ϵkGrid.^2) ≈ 1/4
+    @test kintegrate(kGr, kGr.ϵkGrid.^4) ≈ 5/32
+    @test kintegrate(kGr, kGr.ϵkGrid.^6) ≈ 155/1152
 end
